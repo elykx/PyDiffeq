84827e5d3cd61349f4e6cbda1525da4e2995fbbb
SirC00kie <kostomarov.2001@mail.ru>
2023-01-27 23:07:46 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="PyDiffeq",
+    version="0.0.1",
+    author="Ilya Kostomarov",
+    author_email="",
+    description="A package to solve system ODE",
+    long_description_content_type="text/markdown",
+
+)
+
