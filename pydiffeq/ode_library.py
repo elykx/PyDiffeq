e27c19f4ba4596bb6c4f57092f1885128fe0255c
SirC00kie <kostomarov.2001@mail.ru>
2023-02-07 13:21:06 +0300

    update return solution with decimal place

--- pydiffeq/ode_library.py
+++ pydiffeq/ode_library.py
@@ -13,17 +13,17 @@ class ODE_Library:
         solution = None
 
         if self.method == 'EULER':
-            return EulerMethod(self.system).solve(t, y0)
+            solution = EulerMethod(self.system).solve(t, y0)
         if self.method == 'RK2':
-            return RK2Method(self.system).solve(t, y0)
+            solution = RK2Method(self.system).solve(t, y0)
         if self.method == 'RK4':
-            return RK4Method(self.system).solve(t, y0)
+            solution = RK4Method(self.system).solve(t, y0)
         if self.method == 'IMPLICIT_EULER':
-            return ImplicitEulerMethod(self.system).solve(t, y0)
+            solution = ImplicitEulerMethod(self.system).solve(t, y0)
         if self.method == 'TRAPEZOID':
-            return TrapezoidMethod(self.system).solve(t, y0)
+            solution = TrapezoidMethod(self.system).solve(t, y0)
         if self.method == 'MIDDLE':
-            return MiddlePointMethod(self.system).solve(t, y0)
+            solution = MiddlePointMethod(self.system).solve(t, y0)
 
         if solution is not None:
             return np.round(solution, self.decimal_place)
